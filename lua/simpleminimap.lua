1bc0872dba28623f665a15551d9ca576d59e9869
micahnico <micahnicodemus22@gmail.com>
2021-07-20 00:13:12 -0400

    fix up highlights & general fixes

--- lua/simpleminimap.lua
+++ lua/simpleminimap.lua
@@ -4,14 +4,31 @@ local bufopt = vim.bo
 local winopt = vim.wo
 
 local recent_cache = {}
-local curr_hightlight_match_ids = {}
-local next_highlight_match_id = 77777
 
-local function clear_highlights()
-  for i=1, #curr_hightlight_match_ids do
-    fn.matchdelete(curr_hightlight_match_ids[i])
+local current_highlights = {}
+local curr_highlight_match_id = 77777
+local handling_move = false
+local rendering = false
+
+local function add_highlight(minimap_win_nr, minimap_win_id, target_line)
+  if not current_highlights[minimap_win_nr] then
+    fn.matchaddpos("Title", {target_line}, 200, curr_highlight_match_id, {window = minimap_win_id })
+    current_highlights[minimap_win_nr] = curr_highlight_match_id
+    curr_highlight_match_id = curr_highlight_match_id + 1
   end
-  curr_hightlight_match_ids = {}
+end
+
+local function clear_highlight(minimap_win_nr, minimap_win_id)
+  if current_highlights[minimap_win_nr] then
+    fn.matchdelete(current_highlights[minimap_win_nr], minimap_win_id)
+    current_highlights[minimap_win_nr] = nil
+    curr_highlight_match_id = curr_highlight_match_id - 1
+  end
+end
+
+local function is_blocked(ft, bt)
+  -- TODO: make options for these
+  return ft == 'NvimTree' or ft == 'dashboard' or ft == "TelescopePrompt" or bt == 'prompt'
 end
 
 local function generate_minimap(buf_nr)
@@ -23,27 +40,23 @@ local function generate_minimap(buf_nr)
   local output = fn.execute("w !code-minimap "..fn.expand('%').." -H "..wscale.." -V "..hscale.." --padding 15")
 
   -- save to cache
-  recent_cache[buf_nr] = {
-    win_id = minimap_win_id,
-    win_nr = minimap_win_nr,
-    content = output
-  }
+  recent_cache[buf_nr] = output
 end
 
 local function render_minimap(buf_nr)
-  if recent_cache[buf_nr] == nil then
+  local minimap_win_nr = fn.bufwinnr('-MINIMAP-')
+  if minimap_win_nr == -1 or recent_cache[buf_nr] == nil then
     return
   end
 
-  local minimap_win_nr = recent_cache[buf_nr].win_nr
-  local minimap_content = recent_cache[buf_nr].content
+  rendering = true
 
   -- switch to the minimap window
   vim.cmd(minimap_win_nr.." . ".."wincmd w")
 
   bufopt.modifiable = true
   vim.cmd("silent 1,$delete _")
-  fn.execute("normal! Go"..tostring(minimap_content)) -- add to minimap buffer
+  fn.execute("normal! Go"..tostring(recent_cache[buf_nr])) -- add to minimap
   vim.cmd("silent 1,3delete _")
   vim.cmd("silent $delete _")
   vim.cmd("silent $delete _")
@@ -51,40 +64,45 @@ local function render_minimap(buf_nr)
 
   -- back to the source file
   vim.cmd("wincmd p")
+
+  rendering = false
 end
 
-local function on_scroll()
+local function on_move()
   local minimap_win_nr = fn.bufwinnr('-MINIMAP-')
-  -- return if no minimap open
-  -- should never be the case here, but still make sure
-  if minimap_win_nr == -1 then
+  if handling_move or rendering or minimap_win_nr == -1 or is_blocked(bufopt.filetype, bufopt.buftype) then
     return
   end
+
+  handling_move = true
 
   local curr_total_lns = fn.line("$")
   local curr_line_percent = fn.line(".") / curr_total_lns
   local curr_ft = bufopt.filetype
 
-  vim.cmd("wincmd p")
-  local target_total_lns = fn.line("$")
-  local target_line = math.max(math.floor(target_total_lns * curr_line_percent + 0.5), 1)
-
+  -- go to the line in the source file if in minimap
   if curr_ft == 'minimap' then
-    fn.cursor(tostring(target_line), 0)
-  else
-    clear_highlights()
-    fn.matchaddpos("Title", {target_line}, 200, next_highlight_match_id)
-    curr_hightlight_match_ids[1] = next_highlight_match_id
-    next_highlight_match_id = next_highlight_match_id + 1
+    vim.cmd("wincmd p")
+    local source_total_lns = fn.line("$")
+    local coor_source_line = math.max(math.floor(source_total_lns * curr_line_percent + 0.5), 1)
+    fn.cursor(tostring(coor_source_line), 0)
+    vim.cmd("wincmd p")
   end
 
-  vim.cmd("wincmd p")
+  local minimap_win_id = fn.win_getid(minimap_win_nr)
+  local minimap_total_lns = fn.getwininfo(minimap_win_id)[1].botline
+  local target_line = math.max(math.floor(minimap_total_lns * curr_line_percent + 0.5), 1)
+
+  -- update minimap highlight
+  clear_highlight(minimap_win_nr, minimap_win_id)
+  add_highlight(minimap_win_nr, minimap_win_id, target_line)
+
+  handling_move = false
 end
 
 local function open()
-  -- return if minimap is already open
   local minimap_win_nr = fn.bufwinnr('-MINIMAP-')
-  if minimap_win_nr > -1 then
+  if minimap_win_nr > -1 or is_blocked(bufopt.filetype, bufopt.buftype) then
     return
   end
 
@@ -117,24 +135,21 @@ local function open()
   vim.cmd("wincmd p")
 end
 
-local function update()
+local function on_buf_enter()
   local minimap_win_nr = fn.bufwinnr('-MINIMAP-')
-
-  -- return if no minimap open
-  if minimap_win_nr == -1 then
+  if minimap_win_nr == -1 or bufopt.filetype == 'minimap' or is_blocked(bufopt.filetype, bufopt.buftype) then
     return
   end
 
   local curr_buf_nr = fn.bufnr('%')
 
-  if bufopt.filetype == 'minimap' then
-    return
-  end
-
   if recent_cache[curr_buf_nr] == nil then
     generate_minimap(curr_buf_nr)
   end
   render_minimap(curr_buf_nr)
+
+  -- set the highlights
+  on_move()
 end
 
 local function close()
@@ -151,6 +166,6 @@ end
 return {
   open = open,
   close = close,
-  update = update,
-  on_scroll = on_scroll
+  on_buf_enter = on_buf_enter,
+  on_move = on_move
 }
--- plugin/simpleminimap.vim
+++ plugin/simpleminimap.vim
@@ -3,11 +3,11 @@ if exists('g:loaded_simpleminimap') | finish | endif " prevent loading file twic
 let s:save_cpo = &cpo " save user coptions
 set cpo&vim " reset them to defaults
 
-" command to run our plugin
+" commands & autocmds
 command! SimpleMinimapOpen lua require'simpleminimap'.open()
 command! SimpleMinimapClose lua require'simpleminimap'.close()
-autocmd! BufEnter * lua require'simpleminimap'.update()
-autocmd! FocusGained,CursorMoved,CursorMovedI * lua require('simpleminimap').on_scroll()
+autocmd! BufEnter * lua require'simpleminimap'.on_buf_enter()
+autocmd! CursorMoved,CursorMovedI * lua require'simpleminimap'.on_move()
 
 let &cpo = s:save_cpo " and restore after
 unlet s:save_cpo
